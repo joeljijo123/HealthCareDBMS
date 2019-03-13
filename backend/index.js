88cec3bde4edf8278414da38294455ba41625772
JoelJijo <joeljijo123>
2019-03-13 13:55:01 -0500

    setup

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,44 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
 
+const selectAllUsers = 'SELECT * FROM employee';
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'coogs123',
+    database: 'mydb'
+})
+
+connection.connect(err => {
+    if(err) {
+        return err;
+    }
+});
+
+
 app.use(cors());
+
+app.get('/', (req,res) => {
+    res.send('hello from the products server')
+});
+
+app.get('/users', (req,res) => {
+   connection.query(selectAllUsers,(err, results) => {
+       if(err) {
+           return res.send(err)
+       }
+       else {
+           return res.json({
+               data: results
+           })
+       }
+   });
+});
 
 app.listen(4000, () => {
     console.log(`Producs server listening on port 4000`)
