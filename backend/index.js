15d11c3c206adb66ec16ac8a2b321139fba7354c
JoelJijo <joeljijo123>
2019-03-13 13:21:17 -0500

    all commiting

---
+++ backend/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.listen(4000, () => {
+    console.log(`Producs server listening on port 4000`)
+});
