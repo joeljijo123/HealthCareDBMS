a56f8b683f0309288d6008bb4064767d513c29ee
joeljijo123 <joeljijo123@gmail.com>
2019-03-31 21:17:07 -0500

    Appointments Attachment

--- frontend/src/Components/Appointments/AppointmentHistory.js
+++ frontend/src/Components/Appointments/AppointmentHistory.js
@@ -3,38 +3,83 @@ import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import NewAppointmentForm from './NewAppointmentForm';
-import { Typography } from '@material-ui/core';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import Typography from '@material-ui/core/Typography';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 const styles = theme =>({
     root: {
-      marginLeft: '5%',
-      marginRight: '5%',
-      marginTop: '2%',
-      alignItems: "center",
-      display: "flex",
-      flexDirection: 'column',
-      backgroundColor: "#a09d9d",
-      padding: theme.spacing.unit*3
+        width: '100%',
+        alignItems: "center",
+        display: "flex",
+        flexDirection: 'column',
+        backgroundColor: "#a09d9d",
+        padding: theme.spacing.unit*3
   
     },
+    heading: {
+        fontSize: theme.typography.pxToRem(15),
+        flexBasis: '33.33%',
+        flexShrink: 0,
+      },
+      secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary,
+      },
 });
 
-function NewAppointment(){
-   
-};
 
-function AppointmentHistory(props){
+class AppointmentHistory extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            expanded: null,
+            Appointments: [],
+        };
+    }
+
+    handleChange = panel => (event, expanded) => {
+        this.setState({
+            expanded: expanded ? panel : false,
+        });
+    };
+    render(){
+        const{classes}=this.props;
+        const { expanded } = this.state;
+        return(
+            <div>
+                <NewAppointmentForm/>
+                <div className={classes.root}>
+                    
+                    <ExpansionPanel expanded={expanded === 'panel1'}  onChange={this.handleChange('panel1')}>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>General settings</Typography>
+                            <Typography className={classes.secondaryHeading}>I am an expansion panel</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Typography>
+                            Nulla facilisi. Phasellus sollicitudin nulla et quam mattis feugiat. Aliquam eget
+                            maximus est, id dignissim quam.
+                            </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    
+                </div>
+            </div>
+        );
+    }
     
-    return(
-        <div>
-              <h1>AppointmentHistory Page</h1>
-              <NewAppointmentForm newAppointmentVals={props}/>
-        </div>
-    );
 }
 
 AppointmentHistory.propTypes = {
     classes: PropTypes.object.isRequired,
-  };
+};
   
-  export default withStyles(styles)(AppointmentHistory);
+export default withStyles(styles)(AppointmentHistory);
