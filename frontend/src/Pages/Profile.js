2e5897d6b48aa62cd407fd3b9fd6701f591890de
joeljijo123 <joeljijo123@gmail.com>
2019-04-05 00:09:29 -0500

    Backend for GeneralInfor

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import NavigationBar from '../Components/GeneralComponents/NavigationBar';
 import MedicalInformation from '../Components/Profile/MedicalInformation';
 import GeneralInformation from '../Components/Profile/GeneralInformation';
+import GeneralInfo from '../Components/Profile/GeneralInfo';
 
 class Profile extends Component {
   render() { 
