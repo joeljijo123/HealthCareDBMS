a6dc9d81830dd2ada5ae5442b90226988389047a
joeljijo123 <joeljijo123@gmail.com>
2019-04-10 11:42:52 -0500

    index change

--- backend/index.js
+++ backend/index.js
@@ -139,8 +139,7 @@ app.get('/sexes', (req,res) => {
  //Facility and Doctor should be -1 if you want all 
  app.get('/AppointmentReport/:DooctorID/:FacilityID/:MinDate/:MaxDate', (req,res) => {
     const {DoctorID, FacilityID, MinDate,MaxDate} = req.params;
-    connection.query(`call Clinic_Main.AppointmentsReport(${FacilityID}, ${DoctorID}, ${MinDate},${MaxDate});
-    (${userID});`,(err, results) => {
+    connection.query(`call Clinic_Main.AppointmentsReport(${FacilityID}, ${DoctorID}, ${MinDate},${MaxDate});`,(err, results) => {
         if(err) {
             return res.send(err)
         }
