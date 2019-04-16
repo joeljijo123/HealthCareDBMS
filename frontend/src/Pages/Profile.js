41cf2a773e8d95dcd513f06f8c0c18f8526af30b
Joel Jijo <joeljijo123@gmail.com>
2019-04-15 22:10:00 -0500

    Everything is working

--- frontend/src/Pages/Profile.js
+++ frontend/src/Pages/Profile.js
@@ -4,6 +4,7 @@ import MedicalInformation from '../Components/Profile/MedicalInformation';
 import ProfileInfo from '../Components/Profile/ProfileInfo';
 import { withStyles } from '@material-ui/core';
 import PropTypes from 'prop-types';
+import ProfileInfoTest from '../Components/Profile/ProfileInfoTest';
 
 
 
@@ -21,7 +22,7 @@ class Profile extends Component {
     return (
       <div className={classes.root}>
         <NavigationBar title={"My Profile"} Tab={1}/>
-        <ProfileInfo/>
+        <ProfileInfoTest/>
         {window.localStorage.userType === "2" ? (
           <MedicalInformation/>
         ):(
